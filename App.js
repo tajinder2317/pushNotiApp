54821e0633bcccf963c213ef0a47640246a95aa9
tajinder2317 <soorma2317@gmail.com>
2026-02-04 18:03:05 +0530

    commit 04-02-2026:06.03.PM

--- App.js
+++ App.js
@@ -1,43 +1,109 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { Alert, StyleSheet, Text, View, Button, Platform } from "react-native";
 import * as Notifications from "expo-notifications";
+import { useEffect } from "react";
+import Constants from "expo-constants";
 import * as Device from "expo-device";
-
-async function registerForPushNotificationsAsync() {
-  if (!Device.isDevice)
-    return alert("Use a physical device for push notifications");
-  const { status: existing } = await Notifications.getPermissionsAsync();
-  const { status } =
-    existing === "granted"
-      ? { status: existing }
-      : await Notifications.requestPermissionsAsync();
-  if (status !== "granted") throw new Error("Permission not granted");
-  const token = (await Notifications.getExpoPushTokenAsync()).data;
-  return token;
-}
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
-    shouldShowAlert: true,
     shouldPlaySound: false,
     shouldSetBadge: false,
+    shouldShowBanner: true,
+    shouldShowList: true,
   }),
 });
+
 export default function App() {
+  useEffect(() => {
+    // Notifications.requestPermissionsAsync();
+    async function configurePushNotification() {
+      if (Platform.OS === 'ios' && !Device.isDevice) {
+        Alert.alert("Simulator Detected", "Push Notifications are not supported on iOS Simulators. Please use a physical iOS device.");
+        return;
+      }
+
+      const { status } = await Notifications.getPermissionsAsync();
+      let finalStatus = status;
+
+      if (finalStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+
+      if (finalStatus !== "granted") {
+        Alert.alert(
+          "Permission Required !",
+          "Notifications require user's permission.",
+        );
+        return;
+      }
+      try {
+        const projectId =
+          Constants?.expoConfig?.extra?.eas?.projectId ??
+          Constants?.expoConfig?.projectId;
+        const pushTokenData = await Notifications.getExpoPushTokenAsync({
+          projectId,
+        });
+        console.log(pushTokenData);
+        Alert.alert("Push Token", JSON.stringify(pushTokenData));
+      } catch (error) {
+        console.log("Error fetching push token:", error);
+        Alert.alert("Error fetching push token", error.message);
+      }
+    }
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: "#FF231F7C",
+      });
+    }
+
+    configurePushNotification();
+  }, []);
+
+  useEffect(() => {
+    const subscription1 = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("NOTIFICATION RECEIVED");
+        console.log(notification);
+        const userName = notification.request.content.data.userName;
+        console.log(userName);
+      },
+    );
+    const subscription2 = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log("NOTIFICATION RESPONSE RECEIVED");
+        console.log(response);
+        const userName = response.notification.request.content.data.userName;
+        console.log(userName);
+      },
+    );
+    return () => {
+      subscription1.remove();
+      subscription2.remove();
+    };
+  }, []);
+
   function pushNotificationHandler() {
     Notifications.scheduleNotificationAsync({
       content: {
-        title: "You've got mail! 📬",
-        body: "Here is the notification body",
-        data: { data: "goes here" },
+        title: "Look at that notification",
+        body: "I'm so proud of myself!",
+        data: { userName: "tajinder2317" },
       },
-      trigger: { seconds: 2 },
+      trigger: {
+        type: Notifications.SchedulableTriggerInputTypes.TIME_INTERVAL,
+        seconds: 2,
+      },
     });
   }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Click me" onPress={registerForPushNotificationsAsync} />
+      <Button title="Click me" onPress={pushNotificationHandler} />
       <StatusBar style="auto" />
     </View>
   );
--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,12 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config');
+const { getDefaultConfig } = require("expo/metro-config");
+module.exports = (async () => {
+  const config = await getDefaultConfig(__dirname);
+  // Add customizations here if needed, e.g. modify resolver/sourceExts/assetExts
+  return config;
+})();
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+// Add customizations here if needed, e.g. modify resolver/sourceExts/assetExts
 
 module.exports = config;
+  
